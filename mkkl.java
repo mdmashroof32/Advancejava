e1280f8396745c690bd1a171e9ee495f6a1ed49d
mdmashroof32 <2023479862.md@ug.sharda.ac.in>
2025-02-03 22:37:05 +0530

    Add files via upload

---
+++ mkkl.java
@@ -0,0 +1,22 @@
+package second_project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class mkkl {
+	public static void main(String[] args)throws ClassNotFoundException {
+		try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Mas", "root", "52141asik");
+			System.out.println("created connection");
+			
+			
+			
+			
+			
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
